a3435f85b93753e589b25e85acd60541013d291e
Gregory Perez <gperez@facetec.com>
2019-04-01 11:00:01 -0400

    Update package manager definition to support Swift 4.0

--- Package.swift
+++ Package.swift
@@ -1,13 +1,19 @@
-// swift-tools-version:3.0
+// swift-tools-version:4.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 
 import PackageDescription
 
 var package = Package(
-    name: "Cheetah"
+    name: "Cheetah",
+    products: [
+        .library(
+            name: "Cheetah",
+            targets: ["Cheetah"])
+    ],
+    targets: [
+      .target(
+        name: "Cheetah", dependencies: []
+      )
+    ]
 )
-
-#if swift(>=3.1)
-package.swiftLanguageVersions = [3, 4]
-#endif
